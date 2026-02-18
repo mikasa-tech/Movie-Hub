f2df49804be249c8cbe1dc3020c519296d424d39
mikasa-tech <bhavanimathpati97@gmail.com>
2026-02-19 00:46:59 +0530

    Initial commit Netflix Clone

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+
+function Navbar() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        const scrollListener = () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false);
+        };
+        window.addEventListener("scroll", scrollListener);
+        return () => {
+            window.removeEventListener("scroll", scrollListener);
+        };
+    }, []);
+
+    return (
+        <div className={`nav ${show && "nav_black"}`}>
+            <img
+                className="nav_logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+                alt="Netflix Logo"
+            />
+            <img
+                className="nav_avatar"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+                alt="Netflix Avatar"
+            />
+        </div>
+    );
+}
+
+export default Navbar;
